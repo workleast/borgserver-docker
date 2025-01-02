4df491f3f03f48576e992783b0b74d865139f0af
workleast <117976696+workleast@users.noreply.github.com>
2025-01-02 17:32:56 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ echo "Creating configured directories (from .env file)"
 mkdir ${REPO_DIR} || echo "${REPO_DIR} is already existed"
 mkdir ${SSH_DIR} || echo "${SSH_DIR} is already existed"
 echo "Changing ownership of ${REPO_DIR} and ${SSH_DIR} to 1000:1000"
-sudo chown -R 1000:1000 ${REPO_DIR} ${REPO_DIR}/* ${SSH_DIR} ${SSH_DIR}/* || echo "Failed to change ownership" exit 1
+sudo chown -R 1000:1000 ${REPO_DIR} ${SSH_DIR} ${SSH_DIR}/* || echo "Failed to change ownership" exit 1
 echo "Ownership changed"
