7f4dc60a051e0e01d42b9442f53924d580757dbe
workleast <117976696+workleast@users.noreply.github.com>
2025-01-02 17:13:25 +0700

    Update and rename fix-ownership.sh to init.sh

--- fix-ownership.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-REPO_DIR=$(eval echo $(eval "grep REPO_DIR= .env | cut -d '=' -f2"))
-SSH_DIR=$(eval echo $(eval "grep SSH_DIR= .env | cut -d '=' -f2"))
-echo "Changing ownership of ${REPO_DIR} and ${SSH_DIR} to 1000:1000"
-sudo chown -R 1000:1000 ${REPO_DIR} ${SSH_DIR} ${SSH_DIR}/* || echo "Failed to change ownership" exit 1
-echo "Ownership changed"
